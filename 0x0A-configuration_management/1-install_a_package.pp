b4349836132677f0ec43fa7855970ebc51fe505e
bomanaps <mercy.boma35@yahoo.com>
2022-12-09 19:09:35 -0800

    ‘pip’

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install puppet-lint -v 2.5.0
+# install puppet-lint -v 2.1.0
 
 exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
 }
